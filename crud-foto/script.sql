5a2226e004a49dcdb2aa446c581cf1ac9b9ae146
Kartika Diva Asmara Gita <90994290+divaasmara@users.noreply.github.com>
2023-12-19 18:43:41 +0700

    Add files via upload

---
+++ crud-foto/script.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `siswa` (
+  `id` int AUTO_INCREMENT PRIMARY KEY,
+  `nis` varchar(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jenis_kelamin` varchar(10) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `foto` varchar(200) NOT NULL
+)
